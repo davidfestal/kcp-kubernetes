smarterclayton/kubernetes

NEW: Add a controlplane core group that drops the legacy group ""
Some of the core API group is required for generic control plane
server startup - specifically events and namespaces - and so the
core API group must be installed. Instead of loading it as the
legacy group, attempt to make it a standard API group and adapt
the generic startup code as necessary to enable that.

pkg/controlplane/apis/import_known_versions.go
@@ -1,0 +1,21 @@
+/*
+Copyright 2020 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package apis
+
+import (
+	_ "k8s.io/kubernetes/pkg/controlplane/apis/install"
+)

davidfestal/kcp-kubernetes

pkg/genericcontrolplane/apis/import_known_versions.go
@@ -1,0 +1,21 @@
+/*
+Copyright 2020 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package apis
+
+import (
+	_ "k8s.io/kubernetes/pkg/genericcontrolplane/apis/install"
+)