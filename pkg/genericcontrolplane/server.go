kcp-dev/kubernetes

HACK: Add CRD tenancy...
...This commit hacks CRD tenancy by ensuring that logical clusters are taken in account in:
- CRD-related controllers
- APIServices-related controllers
- Discovery + OpenAPI endpoints

In the current Kubernetes design, those 3 areas are highly coupled and intricated,
which explains why this commit had to hack the code at various levels:
- client-go level
- etcd store level,
- controllers level,
- http handlers level.
While this gives a detailed idea of which code needs to be touched in order to enable CRD tenancy,
a clean implementation would first require some refactoring,
in order to build the required abstraction layers that would allow decoupling those areas.

Signed-off-by: David Festal <dfestal@redhat.com>

pkg/controlplane/server.go
@@ -29,6 +29,7 @@ import (
 
 	"github.com/emicklei/go-restful"
 	extensionsapiserver "k8s.io/apiextensions-apiserver/pkg/apiserver"
+	"k8s.io/apiextensions-apiserver/pkg/controller/openapi/builder"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apiserver/pkg/admission"
 	"k8s.io/apiserver/pkg/authorization/authorizer"
@@ -48,8 +49,11 @@ import (
 	"k8s.io/component-base/version"
 	"k8s.io/klog"
 	aggregatorapiserver "k8s.io/kube-aggregator/pkg/apiserver"
+	"k8s.io/kube-aggregator/pkg/controllers/openapi/aggregator"
+	"k8s.io/kube-openapi/pkg/handler"
 	"k8s.io/kubernetes/pkg/api/controlplanescheme"
 	"k8s.io/kubernetes/pkg/controlplane/apis"
+	"k8s.io/kubernetes/pkg/controlplane/clientutils"
 	"k8s.io/kubernetes/pkg/controlplane/options"
 	generatedopenapi "k8s.io/kubernetes/pkg/generated/openapi"
 	"k8s.io/kubernetes/pkg/kubeapiserver"
@@ -61,6 +65,7 @@ const Include = "kube-control-plane"
 const (
 	etcdRetryLimit    = 60
 	etcdRetryInterval = 1 * time.Second
+	RootClusterName   = "admin"
 )
 
 // Run runs the specified APIServer.  This should never exit.
@@ -73,15 +78,12 @@ func Run(completeOptions completedServerRunOptions, stopCh <-chan struct{}) erro
 		return err
 	}
 
-	prepared, err := server.PrepareRun()
-	if err != nil {
-		return err
-	}
+	prepared := server.PrepareRun()
 	return prepared.Run(stopCh)
 }
 
 // CreateServerChain creates the apiservers connected via delegation.
-func CreateServerChain(completedOptions completedServerRunOptions, stopCh <-chan struct{}) (*aggregatorapiserver.APIAggregator, error) {
+func CreateServerChain(completedOptions completedServerRunOptions, stopCh <-chan struct{}) (*genericapiserver.GenericAPIServer, error) {
 	kubeAPIServerConfig, _, serviceResolver, pluginInitializer, err := CreateKubeAPIServerConfig(completedOptions)
 	if err != nil {
 		return nil, err
@@ -126,7 +128,71 @@ func CreateServerChain(completedOptions completedServerRunOptions, stopCh <-chan
 		}
 	})
 
-	return aggregatorServer, nil
+	// HACK: we don't use the OpenAPI Aggregator of the aggretorServer anymore (only the agregator generic server is prepared)
+	// because the openAPI Agregator isn't compatible with CRD tenancy.
+	// So instead, at the same path, we register our own handler that mainly returns the right merged openAPI schema
+	// based on the logical cluster based on the incoming request.
+	downloader := aggregator.NewDownloader()
+	aggregatorServer.GenericAPIServer.Handler.NonGoRestfulMux.HandleFunc("/openapi/v2", func(res http.ResponseWriter, req *http.Request) {
+		req = req.Clone(req.Context())
+		req.URL.Path = "/openapi/v2"
+		req.URL.RawPath = req.URL.Path
+
+		cluster := genericapirequest.ClusterFrom(req.Context())
+
+		withCluster := func(handler http.Handler) http.HandlerFunc {
+			return func(res http.ResponseWriter, req *http.Request) {
+				if cluster != nil {
+					req = req.Clone(genericapirequest.WithCluster(req.Context(), *cluster))
+				}
+				handler.ServeHTTP(res, req)
+			}
+		}
+
+		// DAVID TODO: try to understand why adding the cluster returns nil here. The default openAPIService should work
+		controlPlaneSpec, etag, httpStatus, err := downloader.Download(kubeAPIServer.GenericAPIServer.Handler.Director, "")
+		klog.Infof(etag, httpStatus)
+
+		crdSpecs, etag, httpStatus, err := downloader.Download(withCluster(apiExtensionsServer.GenericAPIServer.Handler.Director), "")
+		klog.Infof(etag, httpStatus)
+
+		mergedSpecs, err := builder.MergeSpecs(controlPlaneSpec, crdSpecs)
+
+		openAPIVersionedService, err := handler.NewOpenAPIService(mergedSpecs)
+		if err != nil {
+			klog.Errorf("Error while building OpenAPI schema: %v", err)
+		}
+
+		handler := &singlePathHandler{}
+
+		// In order to reuse the kube-openapi API as much as possible, we
+		// register the OpenAPI service in the singlePathHandler
+		err = openAPIVersionedService.RegisterOpenAPIVersionedService("/openapi/v2", handler)
+		if err != nil {
+			klog.Errorf("Error while building OpenAPI schema: %v", err)
+		}
+
+		handler.ServeHTTP(res, req)
+	})
+
+	return aggregatorServer.GenericAPIServer, nil
+}
+
+// singlePathHandler is a dummy PathHandler that mainly allows grabbing a http.Handler
+// from a PathHandler consumer and then being able to use the http.Handler
+// to serve a request.
+type singlePathHandler struct {
+	handler [1]http.Handler
+}
+
+func (sph *singlePathHandler) Handle(path string, handler http.Handler) {
+	sph.handler[0] = handler
+}
+func (sph *singlePathHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	if sph.handler[0] == nil {
+		res.WriteHeader(404)
+	}
+	sph.handler[0].ServeHTTP(res, req)
 }
 
 // CreateKubeAPIServer creates and wires a workable kube-apiserver
@@ -290,6 +356,9 @@ func BuildGenericConfig(
 	genericConfig.LoopbackClientConfig.DisableCompression = true
 
 	kubeClientConfig := genericConfig.LoopbackClientConfig
+
+	clientutils.EnableMultiCluster(genericConfig.LoopbackClientConfig, genericConfig, "apiservices", "customresourcedefinitions")
+
 	clientgoExternalClient, err := clientgoclientset.NewForConfig(kubeClientConfig)
 	if err != nil {
 		lastErr = fmt.Errorf("failed to create real external clientset: %v", err)
@@ -342,7 +411,7 @@ func BuildGenericConfig(
 				cluster.Wildcard = true
 				fallthrough
 			case "":
-				cluster.Name = "admin"
+				cluster.Name = RootClusterName
 			default:
 				if !reClusterName.MatchString(clusterName) {
 					http.Error(w, "Unknown cluster", http.StatusNotFound)

staging/src/k8s.io/apiextensions-apiserver/pkg/apiserver/customresource_discovery_controller.go
@@ -22,12 +22,11 @@ import (
 	"time"
 
 	"k8s.io/klog"
-	"k8s.io/kubernetes/pkg/api/legacyscheme"
+	"k8s.io/kubernetes/pkg/api/controlplanescheme"
 
 	autoscaling "k8s.io/api/autoscaling/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/apimachinery/pkg/version"
@@ -49,7 +48,7 @@ type DiscoveryController struct {
 	crdsSynced cache.InformerSynced
 
 	// To allow injection for testing.
-	syncFn func(version schema.GroupVersion) error
+	syncFn func(clusterGroupVersion discovery.ClusterGroupVersion) error
 
 	queue workqueue.RateLimitingInterface
 }
@@ -75,7 +74,7 @@ func NewDiscoveryController(crdInformer informers.CustomResourceDefinitionInform
 	return c
 }
 
-func (c *DiscoveryController) sync(version schema.GroupVersion) error {
+func (c *DiscoveryController) sync(clusterGroupVersion discovery.ClusterGroupVersion) error {
 
 	apiVersionsForDiscovery := []metav1.GroupVersionForDiscovery{}
 	apiResourcesForDiscovery := []metav1.APIResource{}
@@ -92,7 +91,11 @@ func (c *DiscoveryController) sync(version schema.GroupVersion) error {
 			continue
 		}
 
-		if crd.Spec.Group != version.Group {
+		if crd.Spec.Group != clusterGroupVersion.Group {
+			continue
+		}
+
+		if crd.GetClusterName() != clusterGroupVersion.ClusterName {
 			continue
 		}
 
@@ -120,11 +123,11 @@ func (c *DiscoveryController) sync(version schema.GroupVersion) error {
 					Version:      v.Name,
 				})
 			}
-			if v.Name == version.Version {
+			if v.Name == clusterGroupVersion.Version {
 				foundThisVersion = true
 			}
 			if v.Storage {
-				storageVersionHash = discovery.StorageVersionHash(gv.Group, gv.Version, crd.Spec.Names.Kind)
+				storageVersionHash = discovery.StorageVersionHash(clusterGroupVersion.ClusterName, gv.Group, gv.Version, crd.Spec.Names.Kind)
 			}
 		}
 
@@ -150,7 +153,7 @@ func (c *DiscoveryController) sync(version schema.GroupVersion) error {
 			StorageVersionHash: storageVersionHash,
 		})
 
-		subresources, err := apiextensionshelpers.GetSubresourcesForVersion(crd, version.Version)
+		subresources, err := apiextensionshelpers.GetSubresourcesForVersion(crd, clusterGroupVersion.Version)
 		if err != nil {
 			return err
 		}
@@ -184,36 +187,37 @@ func (c *DiscoveryController) sync(version schema.GroupVersion) error {
 	// HACK: if we are adding resources in legacy scheme group through CRDs (KCP scenario)
 	// then do not expose the CRD `APIResource`s in their own CRD-related group`,
 	// But instead add them in the existing legacy schema group
-	if legacyscheme.Scheme.IsGroupRegistered(version.Group) {
+	if controlplanescheme.Scheme.IsGroupRegistered(clusterGroupVersion.Group) {
 		if !foundGroup || !foundVersion {
-			delete(discovery.ContributedResources, version)
+			delete(discovery.ContributedResources, clusterGroupVersion)
 		}
 
-		discovery.ContributedResources[version] = resourceListerFunc
-	}
-
-	if !foundGroup {
-		c.groupHandler.unsetDiscovery(version.Group)
-		c.versionHandler.unsetDiscovery(version)
+		discovery.ContributedResources[clusterGroupVersion] = resourceListerFunc
 		return nil
 	}
 
-	if version.Group != "" {
+	if !foundGroup {
+		c.groupHandler.unsetDiscovery(clusterGroupVersion.ClusterName, clusterGroupVersion.Group)
+		c.versionHandler.unsetDiscovery(clusterGroupVersion.ClusterName, clusterGroupVersion.GroupVersion())
+		return nil
+	}
+
+	if clusterGroupVersion.Group != "" {
 		// If we don't add resources in the core API group
 		apiGroup := metav1.APIGroup{
-			Name:     version.Group,
+			Name:     clusterGroupVersion.Group,
 			Versions: apiVersionsForDiscovery,
 			// the preferred versions for a group is the first item in
 			// apiVersionsForDiscovery after it put in the right ordered
 			PreferredVersion: apiVersionsForDiscovery[0],
 		}
-		c.groupHandler.setDiscovery(version.Group, discovery.NewAPIGroupHandler(Codecs, apiGroup))
+		c.groupHandler.setDiscovery(clusterGroupVersion.ClusterName, clusterGroupVersion.Group, discovery.NewAPIGroupHandler(Codecs, apiGroup))
 
 		if !foundVersion {
-			c.versionHandler.unsetDiscovery(version)
+			c.versionHandler.unsetDiscovery(clusterGroupVersion.ClusterName, clusterGroupVersion.GroupVersion())
 			return nil
 		}
-		c.versionHandler.setDiscovery(version, discovery.NewAPIVersionHandler(Codecs, version, resourceListerFunc))
+		c.versionHandler.setDiscovery(clusterGroupVersion.ClusterName, clusterGroupVersion.GroupVersion(), discovery.NewAPIVersionHandler(Codecs, clusterGroupVersion.GroupVersion(), resourceListerFunc))
 	}
 
 	return nil
@@ -256,7 +260,7 @@ func (c *DiscoveryController) processNextWorkItem() bool {
 	}
 	defer c.queue.Done(key)
 
-	err := c.syncFn(key.(schema.GroupVersion))
+	err := c.syncFn(key.(discovery.ClusterGroupVersion))
 	if err == nil {
 		c.queue.Forget(key)
 		return true
@@ -270,7 +274,10 @@ func (c *DiscoveryController) processNextWorkItem() bool {
 
 func (c *DiscoveryController) enqueue(obj *apiextensionsv1.CustomResourceDefinition) {
 	for _, v := range obj.Spec.Versions {
-		c.queue.Add(schema.GroupVersion{Group: obj.Spec.Group, Version: v.Name})
+		c.queue.Add(discovery.ClusterGroupVersion{
+			ClusterName: obj.GetClusterName(),
+			Group:       obj.Spec.Group,
+			Version:     v.Name})
 	}
 }
 

davidfestal/kcp-kubernetes

pkg/genericcontrolplane/server.go
@@ -29,6 +29,7 @@ import (
 
 	"github.com/emicklei/go-restful"
 	extensionsapiserver "k8s.io/apiextensions-apiserver/pkg/apiserver"
+	"k8s.io/apiextensions-apiserver/pkg/controller/openapi/builder"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apiserver/pkg/admission"
 	"k8s.io/apiserver/pkg/authorization/authorizer"
@@ -48,8 +49,11 @@ import (
 	"k8s.io/component-base/version"
 	"k8s.io/klog"
 	aggregatorapiserver "k8s.io/kube-aggregator/pkg/apiserver"
+	"k8s.io/kube-aggregator/pkg/controllers/openapi/aggregator"
+	"k8s.io/kube-openapi/pkg/handler"
 	"k8s.io/kubernetes/pkg/api/genericcontrolplanescheme"
 	"k8s.io/kubernetes/pkg/genericcontrolplane/apis"
+	"k8s.io/kubernetes/pkg/genericcontrolplane/clientutils"
 	"k8s.io/kubernetes/pkg/genericcontrolplane/options"
 	generatedopenapi "k8s.io/kubernetes/pkg/generated/openapi"
 	"k8s.io/kubernetes/pkg/kubeapiserver"
@@ -61,6 +65,7 @@ const Include = "kube-control-plane"
 const (
 	etcdRetryLimit    = 60
 	etcdRetryInterval = 1 * time.Second
+	RootClusterName   = "admin"
 )
 
 // Run runs the specified APIServer.  This should never exit.
@@ -73,15 +78,12 @@ func Run(completeOptions completedServerRunOptions, stopCh <-chan struct{}) erro
 		return err
 	}
 
-	prepared, err := server.PrepareRun()
-	if err != nil {
-		return err
-	}
+	prepared := server.PrepareRun()
 	return prepared.Run(stopCh)
 }
 
 // CreateServerChain creates the apiservers connected via delegation.
-func CreateServerChain(completedOptions completedServerRunOptions, stopCh <-chan struct{}) (*aggregatorapiserver.APIAggregator, error) {
+func CreateServerChain(completedOptions completedServerRunOptions, stopCh <-chan struct{}) (*genericapiserver.GenericAPIServer, error) {
 	kubeAPIServerConfig, _, serviceResolver, pluginInitializer, err := CreateKubeAPIServerConfig(completedOptions)
 	if err != nil {
 		return nil, err
@@ -126,7 +128,71 @@ func CreateServerChain(completedOptions completedServerRunOptions, stopCh <-chan
 		}
 	})
 
-	return aggregatorServer, nil
+	// HACK: we don't use the OpenAPI Aggregator of the aggretorServer anymore (only the agregator generic server is prepared)
+	// because the openAPI Agregator isn't compatible with CRD tenancy.
+	// So instead, at the same path, we register our own handler that mainly returns the right merged openAPI schema
+	// based on the logical cluster based on the incoming request.
+	downloader := aggregator.NewDownloader()
+	aggregatorServer.GenericAPIServer.Handler.NonGoRestfulMux.HandleFunc("/openapi/v2", func(res http.ResponseWriter, req *http.Request) {
+		req = req.Clone(req.Context())
+		req.URL.Path = "/openapi/v2"
+		req.URL.RawPath = req.URL.Path
+
+		cluster := genericapirequest.ClusterFrom(req.Context())
+
+		withCluster := func(handler http.Handler) http.HandlerFunc {
+			return func(res http.ResponseWriter, req *http.Request) {
+				if cluster != nil {
+					req = req.Clone(genericapirequest.WithCluster(req.Context(), *cluster))
+				}
+				handler.ServeHTTP(res, req)
+			}
+		}
+
+		// DAVID TODO: try to understand why adding the cluster returns nil here. The default openAPIService should work
+		controlPlaneSpec, etag, httpStatus, err := downloader.Download(kubeAPIServer.GenericAPIServer.Handler.Director, "")
+		klog.Infof(etag, httpStatus)
+
+		crdSpecs, etag, httpStatus, err := downloader.Download(withCluster(apiExtensionsServer.GenericAPIServer.Handler.Director), "")
+		klog.Infof(etag, httpStatus)
+
+		mergedSpecs, err := builder.MergeSpecs(controlPlaneSpec, crdSpecs)
+
+		openAPIVersionedService, err := handler.NewOpenAPIService(mergedSpecs)
+		if err != nil {
+			klog.Errorf("Error while building OpenAPI schema: %v", err)
+		}
+
+		handler := &singlePathHandler{}
+
+		// In order to reuse the kube-openapi API as much as possible, we
+		// register the OpenAPI service in the singlePathHandler
+		err = openAPIVersionedService.RegisterOpenAPIVersionedService("/openapi/v2", handler)
+		if err != nil {
+			klog.Errorf("Error while building OpenAPI schema: %v", err)
+		}
+
+		handler.ServeHTTP(res, req)
+	})
+
+	return aggregatorServer.GenericAPIServer, nil
+}
+
+// singlePathHandler is a dummy PathHandler that mainly allows grabbing a http.Handler
+// from a PathHandler consumer and then being able to use the http.Handler
+// to serve a request.
+type singlePathHandler struct {
+	handler [1]http.Handler
+}
+
+func (sph *singlePathHandler) Handle(path string, handler http.Handler) {
+	sph.handler[0] = handler
+}
+func (sph *singlePathHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	if sph.handler[0] == nil {
+		res.WriteHeader(404)
+	}
+	sph.handler[0].ServeHTTP(res, req)
 }
 
 // CreateKubeAPIServer creates and wires a workable kube-apiserver
@@ -262,7 +328,7 @@ func BuildGenericConfig(
 	kubeVersion := version.Get()
 	genericConfig.Version = &kubeVersion
 
-	storageFactoryConfig := kubeapiserver.NewStorageFactoryConfig(controlplanescheme.Scheme, controlplanescheme.Codecs)
+	storageFactoryConfig := kubeapiserver.NewStorageFactoryConfig(genericcontrolplanescheme.Scheme, genericcontrolplanescheme.Codecs)
 	storageFactoryConfig.APIResourceConfig = genericConfig.MergedResourceConfig
 	completedStorageFactoryConfig, err := storageFactoryConfig.Complete(s.Etcd)
 	if err != nil {
@@ -290,6 +356,9 @@ func BuildGenericConfig(
 	genericConfig.LoopbackClientConfig.DisableCompression = true
 
 	kubeClientConfig := genericConfig.LoopbackClientConfig
+
+	clientutils.EnableMultiCluster(genericConfig.LoopbackClientConfig, genericConfig, "apiservices", "customresourcedefinitions")
+
 	clientgoExternalClient, err := clientgoclientset.NewForConfig(kubeClientConfig)
 	if err != nil {
 		lastErr = fmt.Errorf("failed to create real external clientset: %v", err)
@@ -342,7 +411,7 @@ func BuildGenericConfig(
 				cluster.Wildcard = true
 				fallthrough
 			case "":
-				cluster.Name = "admin"
+				cluster.Name = RootClusterName
 			default:
 				if !reClusterName.MatchString(clusterName) {
 					http.Error(w, "Unknown cluster", http.StatusNotFound)

staging/src/k8s.io/apiextensions-apiserver/pkg/apiserver/customresource_discovery_controller.go
@@ -22,12 +22,11 @@ import (
 	"time"
 
 	"k8s.io/klog"
-	"k8s.io/kubernetes/pkg/api/legacyscheme"
+	"k8s.io/kubernetes/pkg/api/genericcontrolplanescheme"
 
 	autoscaling "k8s.io/api/autoscaling/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/apimachinery/pkg/version"
@@ -49,7 +48,7 @@ type DiscoveryController struct {
 	crdsSynced cache.InformerSynced
 
 	// To allow injection for testing.
-	syncFn func(version schema.GroupVersion) error
+	syncFn func(clusterGroupVersion discovery.ClusterGroupVersion) error
 
 	queue workqueue.RateLimitingInterface
 }
@@ -75,7 +74,7 @@ func NewDiscoveryController(crdInformer informers.CustomResourceDefinitionInform
 	return c
 }
 
-func (c *DiscoveryController) sync(version schema.GroupVersion) error {
+func (c *DiscoveryController) sync(clusterGroupVersion discovery.ClusterGroupVersion) error {
 
 	apiVersionsForDiscovery := []metav1.GroupVersionForDiscovery{}
 	apiResourcesForDiscovery := []metav1.APIResource{}
@@ -92,7 +91,11 @@ func (c *DiscoveryController) sync(version schema.GroupVersion) error {
 			continue
 		}
 
-		if crd.Spec.Group != version.Group {
+		if crd.Spec.Group != clusterGroupVersion.Group {
+			continue
+		}
+
+		if crd.GetClusterName() != clusterGroupVersion.ClusterName {
 			continue
 		}
 
@@ -120,11 +123,11 @@ func (c *DiscoveryController) sync(version schema.GroupVersion) error {
 					Version:      v.Name,
 				})
 			}
-			if v.Name == version.Version {
+			if v.Name == clusterGroupVersion.Version {
 				foundThisVersion = true
 			}
 			if v.Storage {
-				storageVersionHash = discovery.StorageVersionHash(gv.Group, gv.Version, crd.Spec.Names.Kind)
+				storageVersionHash = discovery.StorageVersionHash(clusterGroupVersion.ClusterName, gv.Group, gv.Version, crd.Spec.Names.Kind)
 			}
 		}
 
@@ -150,7 +153,7 @@ func (c *DiscoveryController) sync(version schema.GroupVersion) error {
 			StorageVersionHash: storageVersionHash,
 		})
 
-		subresources, err := apiextensionshelpers.GetSubresourcesForVersion(crd, version.Version)
+		subresources, err := apiextensionshelpers.GetSubresourcesForVersion(crd, clusterGroupVersion.Version)
 		if err != nil {
 			return err
 		}
@@ -184,36 +187,37 @@ func (c *DiscoveryController) sync(version schema.GroupVersion) error {
 	// HACK: if we are adding resources in legacy scheme group through CRDs (KCP scenario)
 	// then do not expose the CRD `APIResource`s in their own CRD-related group`,
 	// But instead add them in the existing legacy schema group
-	if legacyscheme.Scheme.IsGroupRegistered(version.Group) {
+	if genericcontrolplanescheme.Scheme.IsGroupRegistered(clusterGroupVersion.Group) {
 		if !foundGroup || !foundVersion {
-			delete(discovery.ContributedResources, version)
+			delete(discovery.ContributedResources, clusterGroupVersion)
 		}
 
-		discovery.ContributedResources[version] = resourceListerFunc
-	}
-
-	if !foundGroup {
-		c.groupHandler.unsetDiscovery(version.Group)
-		c.versionHandler.unsetDiscovery(version)
+		discovery.ContributedResources[clusterGroupVersion] = resourceListerFunc
 		return nil
 	}
 
-	if version.Group != "" {
+	if !foundGroup {
+		c.groupHandler.unsetDiscovery(clusterGroupVersion.ClusterName, clusterGroupVersion.Group)
+		c.versionHandler.unsetDiscovery(clusterGroupVersion.ClusterName, clusterGroupVersion.GroupVersion())
+		return nil
+	}
+
+	if clusterGroupVersion.Group != "" {
 		// If we don't add resources in the core API group
 		apiGroup := metav1.APIGroup{
-			Name:     version.Group,
+			Name:     clusterGroupVersion.Group,
 			Versions: apiVersionsForDiscovery,
 			// the preferred versions for a group is the first item in
 			// apiVersionsForDiscovery after it put in the right ordered
 			PreferredVersion: apiVersionsForDiscovery[0],
 		}
-		c.groupHandler.setDiscovery(version.Group, discovery.NewAPIGroupHandler(Codecs, apiGroup))
+		c.groupHandler.setDiscovery(clusterGroupVersion.ClusterName, clusterGroupVersion.Group, discovery.NewAPIGroupHandler(Codecs, apiGroup))
 
 		if !foundVersion {
-			c.versionHandler.unsetDiscovery(version)
+			c.versionHandler.unsetDiscovery(clusterGroupVersion.ClusterName, clusterGroupVersion.GroupVersion())
 			return nil
 		}
-		c.versionHandler.setDiscovery(version, discovery.NewAPIVersionHandler(Codecs, version, resourceListerFunc))
+		c.versionHandler.setDiscovery(clusterGroupVersion.ClusterName, clusterGroupVersion.GroupVersion(), discovery.NewAPIVersionHandler(Codecs, clusterGroupVersion.GroupVersion(), resourceListerFunc))
 	}
 
 	return nil
@@ -256,7 +260,7 @@ func (c *DiscoveryController) processNextWorkItem() bool {
 	}
 	defer c.queue.Done(key)
 
-	err := c.syncFn(key.(schema.GroupVersion))
+	err := c.syncFn(key.(discovery.ClusterGroupVersion))
 	if err == nil {
 		c.queue.Forget(key)
 		return true
@@ -270,7 +274,10 @@ func (c *DiscoveryController) processNextWorkItem() bool {
 
 func (c *DiscoveryController) enqueue(obj *apiextensionsv1.CustomResourceDefinition) {
 	for _, v := range obj.Spec.Versions {
-		c.queue.Add(schema.GroupVersion{Group: obj.Spec.Group, Version: v.Name})
+		c.queue.Add(discovery.ClusterGroupVersion{
+			ClusterName: obj.GetClusterName(),
+			Group:       obj.Spec.Group,
+			Version:     v.Name})
 	}
 }
 